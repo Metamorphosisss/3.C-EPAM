32ae77952bee66de91a1c58668e236d045694f63
Ivan <mr.komar991@gmail.com>
2024-03-30 22:41:07 +0200

    epam3

---
+++ 2.1C#EPAM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("введiть число: ");
+        int number = Convert.ToInt32(Console.ReadLine());  
+        int summma = 0;
+        while (number > 0)
+        {
+            summma += number % 10;
+            number /= 10;
+        }
+        Console.WriteLine(summma);
+    }
+}
